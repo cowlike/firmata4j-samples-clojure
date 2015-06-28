3e09596d0cb44b9492187c85647932e369be5e4b
cowlike <cowlike@gmail.com>
2015-06-28 11:37:28 -0500

    add blink to main, fix bug in blink

--- src/firmata4j_samples_clojure/core.clj
+++ src/firmata4j_samples_clojure/core.clj
@@ -1,7 +1,10 @@
 (ns firmata4j-samples-clojure.core
+  (:require [firmata4j-samples-clojure.blink :refer :all])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (condp = (first args)
+    "blink" (blink "COM7" 13)
+    (println "Hello, World!")))
