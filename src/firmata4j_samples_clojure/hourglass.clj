132e0476443f5973f954ae94c17ed542f2b55a6d
cowlike <cowlike@gmail.com>
2015-06-29 23:20:33 -0500

    fix broken formatting

--- src/firmata4j_samples_clojure/hourglass.clj
+++ src/firmata4j_samples_clojure/hourglass.clj
@@ -23,16 +23,16 @@
 ;; then you can set the values of the atoms in the repl and
 ;; turn on the switch-pin (for example) independently from the loop
 (defn hourglass [^IODevice dev]
-(let [led-on #(write-pin dev % 1)
-      led-off #(read-pin dev % 0)]
-  (loop [leds led-pins prev-switch-state 0 prev-time 0]
-    (let [cur-time (.getTime (Date.)) 
-          switch-state (read-pin dev switch-pin)]
-      (if (> (- cur-time prev-time) interval)
-        (do (led-on (first leds))          
-          (recur (next leds) switch-state cur-time))
-        (if (not= switch-state prev-switch-state)
-          (do 
-            (doseq [led leds] (led-off led))
-            (recur led-pins switch-state cur-time))
-          (recur leds prev-switch-state prev-time)))))))
+  (let [led-on #(write-pin dev % 1)
+        led-off #(read-pin dev % 0)]
+    (loop [leds led-pins prev-switch-state 0 prev-time 0]
+      (let [cur-time (.getTime (Date.)) 
+            switch-state (read-pin dev switch-pin)]
+        (if (> (- cur-time prev-time) interval)
+          (do (led-on (first leds))          
+            (recur (next leds) switch-state cur-time))
+          (if (not= switch-state prev-switch-state)
+            (do 
+              (doseq [led leds] (led-off led))
+              (recur led-pins switch-state cur-time))
+            (recur leds prev-switch-state prev-time)))))))
